1bd4d12df1d5b58e65fffe030c388deaf9bf2cf7
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-03-14 07:15:30 -0700

    some changes

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Birdblocks
+ * Plugin Name:       Blackbird Sandbox
  * Description:       bb testing plugin
  * Version:           0.1.0
  * Requires at least: 6.1
@@ -20,79 +20,32 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-// function create_block_birdblocks_block_init() {
-// 	register_block_type( __DIR__ . '/build' );
-// }
+function create_block_birdblocks_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
 // add_action( 'init', 'create_block_birdblocks_block_init' );
 
+add_action( 'init', 'bb_register_block_template' );
 
-add_action( 'init', 'ucsc_register_text_meta' );
-
-function ucsc_register_text_meta() {
-	$fields = array(
-		'code' => 'Code',
-		'form' => 'Form',
-		'aq_code' => 'AQ_Code',
-		'id' => 'ID',
-		'button_text' => 'Fund button text',
-	);
-	foreach ($fields as $slug => $label) {
-		register_post_meta(
-			'fund',
-			$slug,
-			array(
-				'show_in_rest'      => true,
-				'single'            => true,
-				'type'              => 'string',
-				'sanitize_callback' => 'wp_strip_all_tags',
-				'label'							=> _($label),
-			)
-		);
-	}
+function bb_register_block_template() {
+		register_block_template( 'devblog-plugin-templates//my-template', [
+    'title'     => __( 'Example', 'devblog-plugin-templates' ),
+    'description' => __( 'An example block template from a plugin.', 'devblog-plugin-templates' ),
+    'content'   => '
+        <!-- wp:template-part {"slug":"header","area":"header","tagName":"header"} /-->
+        <!-- wp:group {"tagName":"main"} -->
+        <main class="wp-block-group">
+            <!-- wp:group {"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group">
+                <!-- wp:paragraph -->
+                <p>This is a plugin-registered template.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </main>
+        <!-- /wp:group -->
+        <!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer"} /-->'
+] );
 }
 
-// add_action( 'init', 'ucsc_register_url_meta' );
 
-function ucsc_register_url_meta() {
-	$fields = array(
-		'fund_link' => 'Fund link',
-	);
-	foreach ($fields as $slug => $label) {
-		register_post_meta(
-			'fund',
-			$slug,
-			array(
-				'show_in_rest'      => true,
-				'single'            => true,
-				'type'              => 'string',
-				'sanitize_callback' => 'esc_url_raw',
-				'label'							=> _($label),
-			)
-		);
-	}
-}
-
-// add_action( 'wp_head', 'ucsc_test' );
-
-function ucsc_test() {
-	$baseurl = 'https://give.ucsc.edu/campaigns/38026/donations/new?designation=';
-	$aqcode = get_post_meta( get_the_ID(), 'aq_code', true );
-	$fundurl = $baseurl.$aqcode;
-	echo $fundurl;
-}
-
-add_action( 'init', 'ucsc_register_block_bindings' );
-
-function ucsc_register_block_bindings() {
-	register_block_bindings_source( 'ucsc/fund-url', array(
-		'label'              => __( 'Fund URL', 'ucsc' ),
-		'get_value_callback' => 'ucsc_fund_url',
-		// 'uses_context'       => [ 'postId', 'postType' ]
-	) );
-}
-
-function ucsc_fund_url() {
-	$baseurl = 'https://give.ucsc.edu/campaigns/38026/donations/new?designation=';
-	$aqcode = get_post_meta( get_the_ID(), 'aq_code', true );
-	return $baseurl.$aqcode;
-}
